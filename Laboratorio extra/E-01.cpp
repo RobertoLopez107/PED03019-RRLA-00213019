2e27de4db415d3595c77678a8059533dc07072a4
UCA <00213019@uca.edu.sv>
2020-01-13 13:47:41 -0600

    E-01.cpp

---
+++ Laboratorio extra/E-01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    char secret[]= {'c','o','d','i','f','i'.'c','a','r'};
+    char verificacion[9];
+    char guessletter;
+    int a = 13;
+    
+
+    cout<<"Ahorcado"<<endl;
+    cout<<"La palabra adivinada tiene 9 letras"<<endl;
+    do{
+        cout<<"Letra: "; cin>>guessletter;
+
+    }while (a > 0)
+    
+    
+    return 0;
+}
+
+void verifyletter(char arr1[9], char arr2[9], char letter, char size){
+    for(int i = 0; i < size; i++){
+        if(arr1[i]== letter){
+            arr2[i] = letter;
+        }
+    }
+}
